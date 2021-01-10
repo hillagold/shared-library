ccd295d35cb8007a234747b91ab5b3909c634152
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-01-10 12:10:22 +0200

    test

--- vars/artifact.groovy
+++ vars/artifact.groovy
@@ -1,5 +1,6 @@
 def call(def pack,def company, def repo) {
     node {
+         echo "Packaing with canon & Uploading to repo"
          sh "conan new ${pack} -t"
          sh "conan create . ${company}"
          sh "conan upload ${pack}@${company} -r=${repo} --all"
